8b7b5a7b2e980697168944f555d4dc6053a8520d
Gena <138885513+airdropkita@users.noreply.github.com>
2023-07-14 16:48:17 +0300

    Create ramdisk_cache.sh

---
+++ ramdisk_cache.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#    Debian-Systemd-Ramdisk (DSR) is a config repo used to create a ramdisk with systemd.
+#    Copyright (C) 2017  Rémi Ducceschi (remileduc) <remi.ducceschi@gmail.com>
