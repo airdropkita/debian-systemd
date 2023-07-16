da3b86b5420acb54019f096992921e95d75ad1f6
Gena <138885513+airdropkita@users.noreply.github.com>
2023-07-16 14:12:56 +0300

    Update ramdisk_cache.sh

--- ramdisk_cache.sh
+++ ramdisk_cache.sh
@@ -6,3 +6,7 @@
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
+
+declare -A ramdisks=(
+#	["test"]="mounted persistent" # mkdir mounted persistent; touch persistent/test
+	["cache"]="/var/cache /mnt/persistent/system"
