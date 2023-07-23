57975c1ce77f8f5c2d1ea15ba74f9508a8538488
Gena <138885513+airdropkita@users.noreply.github.com>
2023-07-23 17:54:10 +0300

    Update ramdisk_cache.sh

--- ramdisk_cache.sh
+++ ramdisk_cache.sh
@@ -25,3 +25,7 @@ persistent="$2"
 	if [ -e "$mounted"/"$lockfile" ]; then
 rsync -aqu --delete --exclude "$lockfile" "$mounted/" "$persistent/"
 		echo "$(date -Iseconds) - $mounted saved to persistent" >> "$logfile"
+fi
+}
+
+for ramdisk in $@; do
