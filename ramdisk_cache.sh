5a5b8cdabeb041bcdc7cf7bafd38c8e2f45471dc
Gena <138885513+airdropkita@users.noreply.github.com>
2023-08-31 14:46:38 +0300

    Update ramdisk_cache.sh

--- ramdisk_cache.sh
+++ ramdisk_cache.sh
@@ -35,3 +35,6 @@ fi
 for ramdisk in $@; do
 mounted="$(echo ${ramdisks[$ramdisk]} | cut -sd ' ' -f 1)"
 persistent="$(echo ${ramdisks[$ramdisk]} | cut -sd ' ' -f 2)"
+
+	# if wrong parameter, we terminate
+	if [ -z "$mounted" ] || [ -z "$persistent" ]; then
