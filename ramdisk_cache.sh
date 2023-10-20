25e63c70c8000f3fa20c4b267a78984a82b7365f
Gena <138885513+airdropkita@users.noreply.github.com>
2023-10-20 11:53:50 +0200

    Update ramdisk_cache.sh

--- ramdisk_cache.sh
+++ ramdisk_cache.sh
@@ -18,3 +18,7 @@ lockfile=".cache.lock"
 logfile="/tmp/ramdisk_cache.log"
 
 function manageRamdisk { # mounted, persistent
+mounted="$1"
+	persistent="$2"
+
+	# if the lockfile exists in mounted, we save the content in persistent
