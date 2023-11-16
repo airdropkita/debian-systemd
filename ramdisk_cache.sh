ab7e95658b57ef43d9157f20d93693a8138780ef
Gena <138885513+airdropkita@users.noreply.github.com>
2023-11-16 13:08:24 +0100

    Update ramdisk_cache.sh

--- ramdisk_cache.sh
+++ ramdisk_cache.sh
@@ -38,3 +38,4 @@ persistent="$(echo ${ramdisks[$ramdisk]} | cut -sd ' ' -f 2)"
 
 	# if wrong parameter, we terminate
 	if [ -z "$mounted" ] || [ -z "$persistent" ]; then
+	echo "'$ramdisk' is not valid." >> "$logfile"
